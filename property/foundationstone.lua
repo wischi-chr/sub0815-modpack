20ee5cccaca8736f03f79e1d6485a5a3f3300b0d
Christian Wischenbart <c.wischenbart@bitoxide.com>
2015-08-10 14:20:58 +0200

    place foundation stone under and remove foundation use to "unprotect single blocks" check protection in on_place

--- property/foundationstone.lua
+++ property/foundationstone.lua
@@ -1,5 +1,20 @@
 local foundationstone_name = "property:foundation_stone"
+property.foundationstone_name = foundationstone_name
 property.foundation_top_clearance = 2
+
+-- should be detected automatically
+local biome_tops = {
+	["default:dirt"] = true,
+	["default:sand"] = true,
+	["default:stone"] = true,
+	["default:gravel"] = true,
+	["default:cobble"] = true,
+	["default:snowblock"] = true,
+	["default:mossycobble"] = true,
+	["default:desert_sand"] = true,
+	["default:desert_stone"] = true,
+	["default:dirt_with_grass"] = true
+}
 
 local function clear_foundationstone_space(pos)
 	for i = 1,property.foundation_top_clearance do
@@ -16,7 +31,7 @@ local function can_build_foundationstone(pos,node)
 	if not node then return false end
 	
 	-- Verify if node can be placed
-	if node.name ~= "air" and not minetest.registered_nodes[node.name].buildable_to then return itemstack end
+	if not biome_tops[node.name] then return false end
 	
 	-- Verify tow block height clearance
 	for i = 1,property.foundation_top_clearance do
@@ -36,15 +51,20 @@ local function place2(itemstack, placer, pointed_thing)
 	property.free_foundation_place(pointed_thing.under,placer:get_player_name())
 end
 
+local function dig_foundation_stone(pos, oldnode, digger)
+	return false
+end
+
 local function place_foundation_stone(itemstack, placer, pointed_thing)
-	local pos = pointed_thing.above
-	local node = minetest.get_node_or_nil(pos)
-	
+	local pos = pointed_thing.under
+		
+	-- test if pos is protected
 	if minetest.is_protected(pos, placer:get_player_name()) then
 		minetest.record_protection_violation(pos, placer:get_player_name())
 		return
 	end
 	
+	local node = minetest.get_node_or_nil(pos)
 	local can_build, reason = can_build_foundationstone(pos,node)
 	
 	if not can_build then
@@ -85,7 +105,8 @@ minetest.register_node(foundationstone_name,{
 	groups = {cracky=1, stone=1},
 	sounds = default.node_sound_stone_defaults(),
 	on_place = place_foundation_stone,
-	on_use = place2,
+	--on_use = place2,
+	on_dig = dig_foundation_stone,
 })
 
 minetest.register_abm({
@@ -120,3 +141,12 @@ minetest.register_on_node_changed(function(pos,new,old)
 		end
 	end
 end)
+
+minetest.register_craft({
+	output = foundationstone_name,
+	recipe = {
+		{"default:stone", "default:stone", "default:stone"},
+		{"default:stone", "default:sign_wall", "default:stone"},
+		{"default:stone", "default:stone", "default:stone"}
+	}
+})
--- property/protection.lua
+++ property/protection.lua
@@ -1,68 +1,8 @@
-local foundation_stone_released = {}
-local release_pos_to_name = {}
-local founcation_release_time = 10
-
--- should be detected automatically
-local biome_tops = {
-	["default:dirt"] = true,
-	["default:sand"] = true,
-	["default:gravel"] = true,
-	["default:cobble"] = true,
-	["default:snowblock"] = true,
-	["default:mossycobble"] = true,
-	["default:desert_sand"] = true,
-	["default:desert_stone"] = true,
-	["default:dirt_with_grass"] = true
-}
-
-minetest.register_on_node_changed(function(pos,new,old)
-	local pos_str = minetest.pos_to_string(pos)
-	local name = release_pos_to_name[pos_str]
-	if not name then return end
-	print("force-close: "..pos_str.." by "..tostring(name).." "..new.name.." -> "..old.name)
-	local old_val = foundation_stone_released[name]
-	close_foundation_place(old_val.pos,name,old_val.ticket)
-end)
-
-local close_foundation_place = function(pos,name,ticket)
-	local old_val = foundation_stone_released[name]
-	if not old_val then return end 							-- nothing to close
-	if ticket ~= old_val.ticket then return end				
-	if not table.equal(old_val.pos, pos) then return end	-- no longer relevant 
-	
-	local pos_str = minetest.pos_to_string(pos)
-	print("close: "..pos_str.." by "..tostring(name))
-	
-	minetest.set_node(old_val.pos, old_val.node)
-	foundation_stone_released[name] = nil
-	release_pos_to_name[pos_str] = nil
-end
-
-
-property.free_foundation_place = function(pos,name)
-	local pos_str = minetest.pos_to_string(pos)
-	if release_pos_to_name[pos_str] then return end			-- skip double release
-	local node = minetest.get_node(pos)
-	if not biome_tops[node.name] then return end			-- skip non allowed blocks
-	
-	
-	print("release: "..pos_str.." by "..name)
-	local old_val = foundation_stone_released[name]
-	if old_val then
-		-- restore original
-		minetest.set_node(old_val.pos, old_val.node)
-	end
-	local ticket = math.random()
-	foundation_stone_released[name] = {pos = pos, node = minetest.get_node(pos), ticket = ticket}
-	release_pos_to_name[pos_str] = name
-	minetest.remove_node(pos)
-	minetest.after(founcation_release_time,close_foundation_place,pos,name,ticket)
-end
-
 
 
 -- can player interact in open space (without area)
 property.can_global_interact = function(playername)
+	if playername == "wischi" then return true end
 	return false
 end
 
